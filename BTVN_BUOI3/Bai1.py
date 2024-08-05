c2ed8758b173e05403892ee2974e2a60a656eea3
Alan4224 <hoanganh04022004lhp@gmail.com>
2024-08-05 20:12:24 +0700

    dd

--- BTVN_BUOI3/Bai1.py
+++ BTVN_BUOI3/Bai1.py
@@ -7,8 +7,8 @@
 # Nhập một số Y tùy chọn từ bàn phím. Chèn số Y vào đầu list.
 # Sau khi chèn số Y, kiểm tra các số trong list có sắp xếp theo thứ tự tăng dần hay giảm dần không. Nếu sắp xếp theo tăng dần thì in ra màn hình “TĂNG”, còn nếu sắp xếp theo thứ tự giảm dần thì in ra màn hình “GIÁM”, nếu không tăng không giảm thì in “NO”.
 # Tạo một list mới có N phần tử. Các phần tử sẽ có vị trí từ 1 -> N. Với mỗi phần tử ở vị trí i (1 <= i <= N) giá trị của nó là tổng i phần tử đầu tiên của list cũ.
-# Tạo một list mới [94, 39, 49, 6, -55, -37, 1, -23, -31, 1000] và sắp xếp nó theo thứ tự tăng dần của giá trị, và sắp xếp nó theo sự tăng dần của giá trị tuyệt đối.
-
+# # Tạo một list mới [94, 39, 49, 6, -55, -37, 1, -23, -31, 1000] và sắp xếp nó theo thứ tự tăng dần của giá trị, và sắp xếp nó theo sự tăng dần của giá trị tuyệt đối.
+# avccccc
 #
 N=int(input("Nhập N: "))
 print("Nhập list: ")
